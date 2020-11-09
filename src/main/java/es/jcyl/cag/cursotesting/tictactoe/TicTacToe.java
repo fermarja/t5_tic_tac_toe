30483dbafe4c4eab1b5b2e7509f43ced489eb5bd
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-09 22:13:13 +0100

    Implementacion comprobaciones de victoria

--- src/main/java/es/jcyl/cag/cursotesting/tictactoe/TicTacToe.java
+++ src/main/java/es/jcyl/cag/cursotesting/tictactoe/TicTacToe.java
@@ -1,5 +1,8 @@
 package es.jcyl.cag.cursotesting.tictactoe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicTacToe {
 
 	private int TAMANO = 3;
@@ -26,6 +29,10 @@ public class TicTacToe {
 	}
 	
 	public Resultado jugar() {
+		Resultado anterior = comprobarResultado();
+		if (anterior != Resultado.SEGUIR) {
+			throw new RuntimeException("No se puede seguir, la partida terminó con resultado:" + anterior);
+		}
 		Resultado resultado = null;
 		if (turno == Casilla.X) {
 			Posicion p = jugadorX.jugar(clonarTablero());
@@ -54,7 +61,73 @@ public class TicTacToe {
 		else {
 			throw new RuntimeException("Ya ha rellenado la casilla " + x + " "  + y);
 		}
+		return comprobarResultado();
+	}
+	private Resultado comprobarResultado() {
+		
+		// probamos vertical
+		for (int x = 0; x < TAMANO; x++) {
+			if (igualesYNovacias(tablero[x])) {
+				return ganador(tablero[x][0]);
+			}
+		}
+		// Probamos horizontal
+		for (int y = 0; y < TAMANO; y++) {
+			List<Casilla> lista = new ArrayList<Casilla>();
+			for (int x = 0; x < TAMANO; x++) {
+				lista.add(tablero[x][y]);
+			}
+			Casilla[] tmp = lista.toArray(new Casilla[0]);
+			if (igualesYNovacias(tmp)) {
+				return ganador(tmp[0]);
+			}
+		}
+		
+		// Probamos diagonal 1
+		List<Casilla> diagonal1 = new ArrayList<Casilla>();
+		for (int x = 0; x < TAMANO; x++) {
+			diagonal1.add(tablero[x][x]);
+		}
+		Casilla[] tmp2 = diagonal1.toArray(new Casilla[0]);
+		if (igualesYNovacias(tmp2)) {
+			return ganador(tmp2[0]);
+		}
+		
+		// Probamos diagonal 2
+		List<Casilla> diagonal2= new ArrayList<Casilla>();
+		for (int x = 0; x < TAMANO; x++) {
+			diagonal2.add(tablero[x][TAMANO-x-1]);
+		}
+		Casilla[] tmp3 = diagonal2.toArray(new Casilla[0]);
+		if (igualesYNovacias(tmp3)) {
+			return ganador(tmp3[0]);
+		}
+		
+		
+
 		return Resultado.SEGUIR;
+	}
+	
+	private Resultado ganador(Casilla casilla) {
+		if (Casilla.X == casilla) {
+			return Resultado.GANA_X;
+		}
+		else if (Casilla.O == casilla) {
+			return Resultado.GANA_O;
+		}
+		return null;
+	}
+	private boolean igualesYNovacias(Casilla[] lista) {
+		Casilla inicial  =lista[0];
+		if (inicial == Casilla.VACIA) {
+			return false;
+		}
+		for (Casilla casilla : lista) {
+			if (casilla != inicial) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private Casilla[][] clonarTablero() {
